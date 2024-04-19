6a768177ed5f647422f34f8bf93628bc08f627fe
JiSangChoe <chlwltkd123@gmail.com>
2024-04-19 19:41:57 +0900

    문제풀이

---
+++ javaBasic/src/example/Testing4.java
@@ -0,0 +1,29 @@
+package example;
+
+class Parent {
+    String name = "Parent";
+    public Parent() {
+        System.out.print("P; " + this.name);
+    }
+    public Parent(String name) {
+        this.name = name;
+        System.out.print("P(String) :" + this.name);
+    }
+}
+class Child extends Parent {
+    int num;
+    public Child(String name, int num){
+        super(name);
+        this.num = num;
+    }
+    void info() {
+        System.out.print(num);
+    }
+}
+
+public class Testing4 {
+    public static void main(String[] args) {
+        Child c = new Child("Soojebi", 2021);
+        c.info();
+    }
+}
